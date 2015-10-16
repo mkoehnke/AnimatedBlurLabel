7141131fed487fbd1cb7da6bf139253d1ec67cb0
Mathias Koehnke <mathias.koehnke@web.de>
2015-10-16 08:28:23 +0200

    Improved Demo

--- Example/AnimatedBlurLabelDemo/ViewController.swift
+++ Example/AnimatedBlurLabelDemo/ViewController.swift
@@ -11,22 +11,50 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var blurLabel : AnimatedBlurLabel!
-
+    @IBOutlet weak var blurButton : UIButton!
+    @IBOutlet weak var unblurButton : UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         blurLabel.animationDuration = 1.0
         blurLabel.setTitle("AnimatedBlurLabel", subtitle: "Subtitle", alignment: .Center)
     }
-
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        blurLabel.setBlurred(true, animated: true) { finished in
-            self.blurLabel.setBlurred(false, animated: true) { finished in
-                
-            }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("isBlurred: \(blurLabel.isBlurred)")
+        setBlurEnabled(!blurLabel.isBlurred)
+    }
+    
+    @IBAction func blurButtonTouched(sender: AnyObject) {
+        setButtonEnabled(blurButton, enabled: false)
+        setButtonEnabled(unblurButton, enabled: false)
+        blurLabel.setBlurred(true, animated: true) { [weak self] finished in
+            self?.setBlurEnabled(false)
         }
     }
-
+    
+    @IBAction func unblurButtonTouched(sender: AnyObject) {
+        setButtonEnabled(blurButton, enabled: false)
+        setButtonEnabled(unblurButton, enabled: false)
+        blurLabel.setBlurred(false, animated: true) { [weak self] finished in
+            self?.setBlurEnabled(true)
+        }
+        
+    }
+    
+    func setBlurEnabled(enabled: Bool) {
+        setButtonEnabled(blurButton, enabled: enabled)
+        setButtonEnabled(unblurButton, enabled: !enabled)
+    }
+    
+    func setButtonEnabled(button: UIButton, enabled: Bool) {
+        button.enabled = enabled
+        button.alpha = (enabled) ? 1.0 : 0.5
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 }
 
